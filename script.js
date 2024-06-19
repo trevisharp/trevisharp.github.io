fea176319a3ed58acebaf9fbdd70f2803382d7ab
Trevisharp <leonardotrevisansilio@gmail.com>
2024-06-19 11:57:00 -0300

    init animation structure in canvas

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
-window.onload = function()
+const draw = ctx =>
 {
-    let canvas = document.getElementById('mainCanvas');
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
+    
+}
 
-    let ctx = canvas.getContext('2d');
+const clear = ctx =>
+{
     ctx.fillStyle = "#1a1a26";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+const frame = ctx =>
+{
+    clear(ctx)
+    draw(ctx);
+    requestFrame(ctx);
+}
+
+const requestFrame = ctx =>
+    window.requestAnimationFrame(() => frame(ctx))
+
+window.onload = function()
+{
+    canvas = document.getElementById('mainCanvas');
+    ctx = canvas.getContext('2d');
+    requestFrame(ctx);
 }
